63273da5a5587846a32868c35349c1822abef9e7
marlrero <mrtj0329@gmail.com>
2021-09-08 23:17:01 +0900

    first

--- hello.py
+++ hello.py
@@ -1,3 +1,17 @@
 # %%
 print('hello world!')
 # %%
+import numpy as np
+import matplotlib.pyplot as plt
+# %%
+years = [2015, 2016, 2017, 2018, 2019]
+rank = [5, 3, 2, 3, 4]
+# %%
+plt.plot(
+    years,  # x축 데이터
+    rank,  # y축 데이터
+    color='blue',  # 선 색깔
+    marker='o',  # 꼭짓점 설정
+    linestyle='solid'  # 선 스타일 설정
+)
+plt.show()
