03e936262b41738f4ce8344ee53ec728fd568b40
marlrero <mrtj0329@gmail.com>
2021-09-24 18:05:30 +0900

    210924

--- chap01/perceptron.py
+++ chap01/perceptron.py
@@ -15,6 +15,7 @@ AND(0, 0), AND(0, 1), AND(1, 0), AND(1, 1)
 import numpy as np
 
 # %%
+# numpy로 AND 게이트 만들어보기
 x = np.array([0, 1])      # input
 w = np.array([0.5, 0.5])  # weight
 b = -0.7
@@ -23,6 +24,19 @@ print(w*x)             # 0*0.5   1*0.5
 print(np.sum(w*x))     #   0   +  0.5         = 0.5
 print(np.sum(w*x) + b) #   0   +  0.5  - 0.7  = -0.2
 
+# %%
+def AND(x1, x2):   # AND를 numpy를 이용해 재정의
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+
+    if np.sum(w * x) + b <= 0:
+        return 0
+    else:
+        return 1
+
+# %%
+AND(0, 0), AND(0, 1), AND(1, 0), AND(1, 1)
 # %%
 def NAND(x1, x2):
     x = np.array([x1, x2])
@@ -49,15 +63,26 @@ def OR(x1, x2):
         return 0
     else:
         return 1
-
 # %%
 OR(0, 0), OR(0, 1), OR(1, 0), OR(1, 1)
 
 # %%
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+
+    return y
+
+# %%
+XOR(0, 0), XOR(0, 1), XOR(1, 0), XOR(1, 1)
+# %%
 if __name__ == '__main__':
     def main():
+        print('main 함수 start!')
         print(AND(0, 0), AND(0, 1), AND(1, 0), AND(1, 1))
         print(NAND(0, 0), NAND(0, 1), NAND(1, 0), NAND(1, 1))
         print(OR(0, 0), OR(0, 1), OR(1, 0), OR(1, 1))
+        print(XOR(0, 0), XOR(0, 1), XOR(1, 0), XOR(1, 1))
 
     main()
